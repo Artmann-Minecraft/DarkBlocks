c72f18830cc063b78e4bc232c453ee39fb018796
LartyHD <LartyHD@minewar.de>
2018-02-05 21:51:19 +0100

    Update

--- Cores/src/net/darkblocks/cores/listener/CountdownListener.java
+++ Cores/src/net/darkblocks/cores/listener/CountdownListener.java
@@ -235,7 +235,7 @@ public class CountdownListener implements Listener
 			for (GameTeam gameTeam : this.teamManager.getTeams())
 			{
 				String name = gameTeam.getName();
-				cores.add(new Core(coreNames, new Location(Bukkit.getWorld(configuration.getString("Cores." + name + "." + coreNames + ".World")), configuration.getDouble("spawns." + name + "." + coreNames + "X"), configuration.getDouble("spawns." + name + "." + coreNames + "Y"), configuration.getDouble("spawns." + name + "." + coreNames + "Z"), (float) configuration.getDouble("spawns." + name + "." + coreNames + "yaw"), (float) configuration.getDouble("spawns." + name + "." + coreNames + "pitch")), gameTeam));
+				cores.add(new Core(coreNames, new Location(Bukkit.getWorld(configuration.getString("Cores." + name + "." + coreNames + ".World")), configuration.getDouble("Cores." + name + "." + coreNames + "X"), configuration.getDouble("Cores." + name + "." + coreNames + "Y"), configuration.getDouble("Cores." + name + "." + coreNames + "Z")), gameTeam));
 			}
 		}
 		this.coreManager = new CoreManager(this.javaPlugin, cores, this.teamManager, this.spectatorManager, this.gameController);
