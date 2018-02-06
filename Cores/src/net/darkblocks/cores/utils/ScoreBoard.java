4fac1559b0f8fdef09feab84ef9853fa3422ecc5
LartyHD <LartyHD@minewar.de>
2018-02-06 05:54:43 +0100

    Update

--- Cores/src/net/darkblocks/cores/utils/ScoreBoard.java
+++ Cores/src/net/darkblocks/cores/utils/ScoreBoard.java
@@ -3,12 +3,13 @@ package net.darkblocks.cores.utils;
 import net.darkblocks.dark.spigot.team.GameTeam;
 import net.darkblocks.dark.spigot.utils.ScoreBoardUtils;
 import net.minecraft.server.v1_8_R3.ScoreboardScore;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static net.darkblocks.dark.universal.messages.Colors.IMPORTANT;
 import static net.darkblocks.dark.universal.messages.Colors.TEXT;
@@ -20,31 +21,39 @@ public class ScoreBoard extends ScoreBoardUtils
 {
 	public static void update(Player player, String displayName, List<Core> cores, int kills)
 	{
-		List<ScoreboardScore> score = new ArrayList<>();
-		int i = 0;
+		Set<GameTeam> teams = new HashSet<>();
 		for (Core core : cores)
 		{
-			GameTeam gameTeam = core.getGameTeam();
-			ChatColor chatColor = gameTeam.getChatColor();
-			score.add(setScoreboardScore(chatColor + "Team " + gameTeam.getName() + " " + IMPORTANT + "[" + TEXT + gameTeam.getPlayers().size() + IMPORTANT + "]", 4 + i));
-			i++;
-			if (core.isAttacked())
+			teams.add(core.getGameTeam());
+		}
+		int size = cores.size() / teams.size();
+		List<ScoreboardScore> score = new ArrayList<>();
+		score.add(setScoreboardScore(" ", 15));
+		int i = 15;
+		for (GameTeam gameTeam : teams)
+		{
+			i--;
+			score.add(setScoreboardScore(gameTeam.getChatColor() + "Team " + gameTeam.getName() + " " + IMPORTANT + "[" + TEXT + gameTeam.getPlayers().size() + IMPORTANT + "]", i));
+			for (Core core : cores)
 			{
-				if (core.getLocation().getBlock().getType() == Material.BEACON)
+				i--;
+				if (core.isAttacked())
 				{
-					score.add(setScoreboardScore(IMPORTANT + "§e⚠" + TEXT + chatColor + core.getName(), 3 + i));
+					if (core.getLocation().getBlock().getType() == Material.BEACON)
+					{
+						score.add(setScoreboardScore(IMPORTANT + "§e⚠" + TEXT + gameTeam.getChatColor() + core.getName(), i));
+					}
+					else
+					{
+						score.add(setScoreboardScore(IMPORTANT + "§c✖" + TEXT + gameTeam.getChatColor() + core.getName(), i));
+					}
 				}
-				else
+				else if (core.getLocation().getBlock().getType() == Material.BEACON)
 				{
-					score.add(setScoreboardScore(IMPORTANT + "§c✖" + TEXT + chatColor + core.getName(), 3 + i));
+					score.add(setScoreboardScore(IMPORTANT + "§a✔" + TEXT + gameTeam.getChatColor() + core.getName(), i));
 				}
-			}
-			else if (core.getLocation().getBlock().getType() == Material.BEACON)
-			{
-				score.add(setScoreboardScore(IMPORTANT + "§a✔" + TEXT + chatColor + core.getName(), 3 + i));
 			}
 		}
-		score.add(setScoreboardScore(" ", i + 4));
 		score.add(setScoreboardScore("  ", 2));
 		score.add(setScoreboardScore(TEXT + "Kills" + IMPORTANT + ":", 1));
 		score.add(setScoreboardScore("" + IMPORTANT + kills, 0));
